9d7a4b40a56c9ca1fa61576a91fcaeb8d003d85a
Scarv3s <zayed.mzr@gmail.com>
2018-03-31 23:50:50 +0400

    pyprogramming

--- GuiTest.py
+++ GuiTest.py
@@ -1,19 +1,8 @@
 #!/usr/bin/python3
+ import tkinter import *
 
-import tkinter as tk
+ class Window(Frame):
 
-root = tk.Tk()
-logo = tk.PhotoImage(file="giphy.gif")
-
-w1 = tk.Label(root, image=logo).pack(side="right")
-
-explanation = """At present, only GIF and PPM/PGM
-formats are supported, but an interface
-exists to allow additional image file
-formats to be added easily."""
-
-w2 = tk.Label(root,
-              justify=tk.LEFT,
-              padx = 10,
-              text=explanation).pack(side="left")
-root.mainloop()
+     def __init__(self, master=None):
+         Frame.__init__(self, master)
+         self.master = master
